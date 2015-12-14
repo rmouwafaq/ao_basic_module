d7863bc9a5ac2d6386eccffb3a0b8b3eb3848c50
rmouwafaq <github@agilorg.com>
2015-12-14 16:41:06 +0000

    IMP: declarations communes

---
+++ ao_declare.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+ 
+ 
+FIELD_TYPE_SELECTION = [('String','String'),
+                        ('string','String'),
+                        ('Double','Double'),
+                        ('double','Double'),
+                        ('numeric','Numeric'),
+                        ('total','Total'),
+                        ('Integer','Integer'),
+                        ('Date','Date'),
+                        ('date','Date'),
+                        ('Datetime','Datetime'),
+                        ('datetime','Datetime'),
+                        ('Time','Time'),
+                        ('time','Time'),
+                        ]
+
+FIELD_STYLE_SELECTION = [('edit','Edit Texte'),
+                         ('date','Calendrier'),
+                         ('checkbox','Checkbox'),
+                         ('select','Select'),
+                         ('dropdown','Dropdown')
+                        ]
+
+NUMERIC_SELECTION = ['numeric','total','Double','Integer']
+
+CELL_FORMAT_SELECTION = [('',' '),
+                        ('text','Texte'),
+                        ('F2', 'Entier (2) '),
+                        ('F3', 'Entier (3) '),
+                        ('F4', 'Entier (4) '),
+                        ('F5', 'Entier (5) '),
+                        ('F6', 'Double (6) '),
+                        ('F7', 'Double (7) '),
+                        ('F8', 'Double (8) '),
+                        ('F9', 'Double (9) '),
+                        ('F10','Double 8+2'),
+                        ('F11','Double 9+2'),
+                        ('F12','Double 10+2'),
+                        ('F13','Double 11+2'),
+                        ('F14','Double 12+2'),
+                        ('F15','Double 13+2')]
+
+CELL_TYPE_SELECTION =[('text','Texte'),
+                      ('numeric','Numeric'),
+                      ('total','Total'),
+                      ('date','Date'),
+                      ('checkbox','Checkbox'),
+                      ('select','Select'),
+                      ('dropdown','Dropdown'),
+                      ]
